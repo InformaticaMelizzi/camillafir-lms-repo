740a9780c849438130c24c5c6ac1b7d24883ce76
Dave Mellow <davemellow@MacBook-Air-di-Mara.local>
2025-09-20 13:31:43 +0200

    Fix struttura: Plugin.pm con namespace Plugins::CamillaFIR::Plugin e install.xml coerente

--- packages/CamillaFIR/Plugins/CamillaFIR/Plugin.pm
+++ packages/CamillaFIR/Plugins/CamillaFIR/Plugin.pm
@@ -3,16 +3,10 @@ package Plugins::CamillaFIR::Plugin;
 use strict;
 use warnings;
 
-use Slim::Utils::Log;
+use base qw(Slim::Plugin::Base);
 
-my $log = logger('plugin.camillafir');
-
-sub initPlugin {
-    $log->info("CamillaFIR plugin initialized OK");
-}
-
-sub shutdownPlugin {
-    $log->info("CamillaFIR plugin stopped");
+sub getDisplayName {
+    return 'CamillaFIR DSP';
 }
 
 1;
